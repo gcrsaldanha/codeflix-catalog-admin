4728351d00c3569f7afb4a4f540cee1b68fc41b5
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-01-02 13:01:25 -0300

    Fim modulo 3

--- src/core/category/application/use_cases/create_category.py
+++ src/core/category/application/use_cases/create_category.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from uuid import UUID
 from src.core.category.application.category_repository import CategoryRepository
-from src.core.category.application.exceptions import InvalidCategoryData
+from src.core.category.application.use_cases.exceptions import InvalidCategoryData
 
 from src.core.category.domain.category import Category
 
