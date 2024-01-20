24190d78f2742abb50415ed27551ba3085ed825e
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-01-20 19:40:41 -0300

    Add DjangoORMCategoryRepository

---
+++ src/django_project/category_app/repository.py
@@ -0,0 +1,42 @@
+from uuid import UUID
+from src.core.category.domain.category_repository import CategoryRepository
+from src.core.category.domain.category import Category
+from django_project.category_app.models import Category as CategoryORM
+
+
+class DjangoORMCategoryRepository(CategoryRepository):
+    def __init__(self, model: CategoryORM | None = None):
+        self.model = model or CategoryORM
+
+    def save(self, category: Category) -> None:
+        self.model.objects.create(
+            id=category.id,
+            name=category.name,
+            description=category.description,
+            is_active=category.is_active,
+        )
+
+    def get_by_id(self, id: UUID) -> Category | None:
+        try:
+            category_model = self.model.objects.get(id=id)
+            return Category(
+                id=category_model.id,
+                name=category_model.name,
+                description=category_model.description,
+                is_active=category_model.is_active,
+            )
+        except self.model.DoesNotExist:
+            return None
+
+    def delete(self, id: UUID) -> None:
+        self.model.objects.filter(id=id).delete()
+
+    def list(self) -> list[Category]:
+        return [
+            Category(
+                id=category.id,
+                name=category.name,
+                description=category.description,
+                is_active=category.is_active,
+            ) for category in self.model.objects.all()
+        ]
---
+++ src/django_project/category_app/tests/test_repository.py
@@ -0,0 +1,25 @@
+import pytest
+from django_project.category_app.repository import DjangoORMCategoryRepository
+from django_project.category_app.models import Category as CategoryORM
+from core.category.domain.category import Category
+
+
+@pytest.mark.django_db
+class TestSave:
+    def test_saves_category_in_database(self):
+        category = Category(
+            name="Movie",
+            description="Movie description",
+        )
+        repository = DjangoORMCategoryRepository()
+
+        assert CategoryORM.objects.count() == 0
+        repository.save(category)
+        assert CategoryORM.objects.count() == 1
+        saved_category = CategoryORM.objects.get()
+
+        assert saved_category.id == category.id
+        assert saved_category.name == category.name
+        assert saved_category.description == category.description
+        assert saved_category.is_active == category.is_active
+
