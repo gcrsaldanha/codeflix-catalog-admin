3364ec323eb53b910235f8c122b229d48454be36
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-01-20 19:35:03 -0300

    Adicionna Django Category Model

--- src/django_project/category_app/admin.py
+++ src/django_project/category_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django_project.category_app.models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
--- src/django_project/category_app/models.py
+++ src/django_project/category_app/models.py
@@ -1,3 +1,16 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "category"
+
+    def __str__(self):
+        return self.name
