024b9bb1f0bd8c59f4d3ab01cf69ad0c5014f3a9
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-01-29 09:48:28 -0300

    Add test to CreateCategorySerializer

--- src/django_project/category_app/tests/test_serializers.py
+++ src/django_project/category_app/tests/test_serializers.py
@@ -10,43 +10,28 @@ class TestCreateCategoryRequestSerializer:
                 "description": "Categoria para filmes",
             }
         )
-        is_valid = serializer.is_valid()
 
-        assert is_valid is True
+        assert serializer.is_valid() is True
 
-    def test_when_is_active_is_not_provided_then_do_not_add_it_to_serializer(self):
+    def test_when_is_active_is_not_provided_and_partial_then_do_not_add_it_to_serializer(self):
         serializer = CreateCategoryRequestSerializer(
             data={
                 "name": "Filme",
                 "description": "Categoria para filmes",
-            }
+            },
+            partial=True,
         )
-        serializer.is_valid()
 
+        assert serializer.is_valid() is True
         assert "is_active" not in serializer.validated_data
 
-    def test_when_is_active_not_provided_but_called_with_query_dict_then_include_it(self):
-        data = QueryDict(mutable=True)
-        data.update({
-            "name": "Filme",
-            "description": "Categoria para filmes",
-        })
+    def test_when_is_active_is_not_provided_and_not_partial_then_set_to_true(self):
+        serializer = CreateCategoryRequestSerializer(
+            data={
+                "name": "Filme",
+                "description": "Categoria para filmes",
+            },
+        )
+        assert serializer.is_valid() is True
 
-        serializer = CreateCategoryRequestSerializer(data=data)
-        serializer.is_valid()
-
-        assert "is_active" in serializer.validated_data
-        assert serializer.validated_data["is_active"] is False
-
-
-    def test_when_called_with_query_dict_but_partial_then_do_not_include_is_active(self):
-        data = QueryDict(mutable=True)
-        data.update({
-            "name": "Filme",
-            "description": "Categoria para filmes",
-        })
-
-        serializer = CreateCategoryRequestSerializer(data=data, partial=True)
-        serializer.is_valid()
-
-        assert "is_active" not in serializer.validated_data
+        assert serializer.validated_data["is_active"] is True
