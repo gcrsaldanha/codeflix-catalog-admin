725b4149c5893f52b066794de13d0a9b2d59199e
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-03-20 08:37:55 -0300

    Remove events

--- src/core/_shared/domain/entity.py
+++ src/core/_shared/domain/entity.py
@@ -3,7 +3,6 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from uuid import UUID
 
-from src.core._shared.domain.event import DomainEvent, EventServiceInterface
 from src.core._shared.domain.notification import Notification
 
 
@@ -11,7 +10,6 @@ from src.core._shared.domain.notification import Notification
 class Entity(ABC):
     id: UUID = field(default_factory=uuid.uuid4)
     notification: Notification = field(default_factory=Notification, init=False)
-    events: list[DomainEvent] = field(default_factory=list, init=False)
 
     def __eq__(self, other: "Entity") -> bool:
         if not isinstance(other, self.__class__):
@@ -22,12 +20,3 @@ class Entity(ABC):
     @abstractmethod
     def validate(self):
         pass
-
-    def add_event(self, event: DomainEvent) -> None:
-        self.events.append(event)
-
-    def dispatch_events(self, event_bus: EventServiceInterface) -> None:
-        for event in self.events:
-            event_bus.send(event)
-
-        self.events = []
--- src/core/video/domain/events.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from src.core._shared.domain.event import DomainEvent
-
-
-@dataclass(frozen=True, slots=True)
-class AudioVideoMediaUpdated(DomainEvent):
-    pass
